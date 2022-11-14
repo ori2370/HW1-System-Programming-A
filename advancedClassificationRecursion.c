4864a35c993ecf02552aa4ce6b971c6863bffe34
nitayGitHub <nitaylevi1998@gmail.com>
2022-11-14 21:15:20 +0200

    new update

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,3 +1,17 @@
-//
-// Created by ori on 11/14/22.
-//
+#include <stdio.h>
+#include "NumClass.h"
+
+int isPalindrome2(int n, int first,int last, int a) {
+    if(n < 1){return 1;}
+    a = power(10, sumOfDigits(n)-1);
+    first = n % 10;
+    last = n / a;
+    if (first != last){
+        return isPalindrome2( ((n%a)-first)/10, 0,0,0) * 0;
+    }
+    return isPalindrome2( ((n%a)-first)/10, 0,0,0)*1;
+}
+
+int isPalindrome1(int n) {
+    return isPalindrome2(n, 0, 0, 0);
+}
