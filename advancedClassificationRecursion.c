756b3dea1e8121c05ebf65a81c3c0f92978c7dee
ori <ori23700@gmail.com>
2022-11-14 21:42:30 +0200

    try of the isArmstrong function

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,3 +1,37 @@
-//
-// Created by ori on 11/14/22.
-//
+#include <stdio.h>
+#include "NumClass.h"
+
+
+//subfunctions that help with the main functions
+int power2(int n,int m){
+    int pow=1;
+    for (int i = 1; i <=m ; i++) {
+        pow*=n;
+    }
+    return pow;
+}
+int sumOfDigits2(int n){
+    int sum=0,tmp=n;
+    do {
+        tmp/=10;
+        sum++;
+    } while (tmp>0);
+    return sum;
+}
+
+int isArmstrong2(int n,int m){
+ if(n<1)return 0;
+   int sum= power2(n%10,m);
+    return sum+isArmstrong2(n/10,m);
+}
+int isArmstrong(int n) {
+    if(isArmstrong2(n, sumOfDigits2(n)-1) ==n) return 1;
+    return 0;
+    }
+
+int main(){
+    printf("badok ahi %d" , isArmstrong(407));
+}
+
+
+
