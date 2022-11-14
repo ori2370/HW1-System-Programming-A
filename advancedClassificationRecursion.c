94383956be9f295cba6f5f5aa2be2a0ebffdaca9
ori2370 <102467907+ori2370@users.noreply.github.com>
2022-11-14 21:56:46 +0200

    Update advancedClassificationRecursion.c
    update the names of the functions

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -16,35 +16,15 @@ int isPalindrome1(int n) {
     return isPalindrome2(n, 0, 0, 0);
 }
 
-//subfunctions that help with the main functions
-int power2(int n,int m){
-    int pow=1;
-    for (int i = 1; i <=m ; i++) {
-        pow*=n;
-    }
-    return pow;
-}
-int sumOfDigits2(int n){
-    int sum=0,tmp=n;
-    do {
-        tmp/=10;
-        sum++;
-    } while (tmp>0);
-    return sum;
-}
 
 int isArmstrong2(int n,int m){
     if(n<1)return 0;
-    int sum= power2(n%10,m);
+    int sum= power(n%10,m);
     return sum+isArmstrong2(n/10,m);
 }
 int isArmstrong(int n) {
-    if(isArmstrong2(n, sumOfDigits2(n)-1) ==n) return 1;
+    if(isArmstrong2(n, sumOfDigits(n)) ==n) return 1;
     return 0;
-}
-
-int main(){
-    printf("badok ahi %d" , isArmstrong(407));
 }
 
 
