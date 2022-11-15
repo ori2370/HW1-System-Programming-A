9746ea0f971e2f84dcf5cefd880a7aaec60a3efb
ori <ori23700@gmail.com>
2022-11-15 21:37:13 +0200

    updating the main class

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "NumClass.h"
+
+int main(){
+    int first,second;
+    printf("please enter two numbers \n");
+    scanf("%d%d", &first, &second);
+    if(second<first){
+        int tmp=first;
+        int first=second;
+        imt second=tmp;
+    }
+    printf("the prime numbers are:")
+    for(int i=first;i<=second:i++;){
+        if(isPrime(i)) {
+            printf("%d" ,i);
+        }
+    }
+    printf("\n");
+    printf("the armstrong numbers are:")
+    for(int i=first;i<=second:i++;){
+        if(isArmstrong(i)) {
+            printf("%d" ,i);
+        }
+    }
+    printf("\n");
+    printf("the strong numbers are:")
+    for(int i=first;i<=second:i++;){
+        if(isStrong(i)) {
+            printf("%d" ,i);
+        }
+    }
+    printf("\n");
+    printf("the palindrome numbers are:")
+    for(int i=first;i<=second:i++;){
+        if(isPalindrome(i)) {
+            printf("%d" ,i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
