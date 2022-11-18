b259a196607504648b601f59c59e3d667a091671
nitayGitHub <nitaylevi1998@gmail.com>
2022-11-18 12:01:42 +0200

    space fix

--- main.c
+++ main.c
@@ -13,28 +13,32 @@ int main(){
     printf("The Armstrong numbers are: ");
     for(int i=first;i<=second;i++){
         if(isArmstrong(i)) {
-            printf("%d ",i);
+            if(i!=first){printf(" ");}
+            printf("%d",i);
         }
     }
     printf("\n");
     printf("The Palindromes are: ");
     for(int i=first;i<=second;i++){
         if(isPalindrome(i)) {
-            printf("%d " ,i);
+            if(i!=first){printf(" ");}
+            printf("%d" ,i);
         }
     }
     printf("\n");
     printf("The Prime numbers are: ");
     for(int i=first;i<=second;i++){
         if(isPrime(i)) {
-            printf("%d " ,i);
+            if(i!=first){printf(" ");}
+            printf("%d" ,i);
         }
     }
     printf("\n");
     printf("The Strong numbers are: ");
     for(int i=first;i<=second;i++){
         if(isStrong(i)) {
-            printf("%d ",i);
+            if(i!=first){printf(" ");}
+            printf("%d",i);
         }
     } 
     printf("\n");
