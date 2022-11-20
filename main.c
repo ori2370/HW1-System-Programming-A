525dc68b6c8c0e6b341fa0dc246b6cba4bfe28ad
ori <ori23700@gmail.com>
2022-11-20 09:45:02 +0200

    try

--- main.c
+++ main.c
@@ -11,45 +11,35 @@ int main() {
         first = second;
         second = tmp;
     }
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = first; i <= second; i++) {
         if (isArmstrong(i)) {
-            if (i == second) {
-                printf("%d", i);
-            } else
-                printf("%d ", i);
+                printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for (int i = first; i <= second; i++) {
         if (isPalindrome(i)) {
-            if (i == second) {
-                printf("%d", i);
-            } else
-                printf("%d ", i);
+                printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for (int i = first; i <= second; i++) {
         if (isPrime(i)) {
-            if (i == second) {
-                printf("%d", i);
-            } else
-                printf("%d ", i);
+                printf(" %d", i);
         }
     }
     printf("\n");
-    printf("The Strong numbers are: ");
+    printf("The Strong numbers are:");
     for (int i = first; i <= second; i++) {
         if (isStrong(i)) {
-            if (i == second) {
-                printf("%d", i);
-            } else
-                printf("%d ", i);
+            printf(" %d ", i);
         }
-        printf("\n");
+        if (i == second) {
+            printf("\n");
+        }
     }
-    return 0;
+        return 0;
 }
